1b273d5f3490b6a0d1c531e0a829c0da59884581
Rakha Galih <rakha_galih_28rpl@student.smktelkom-mlg.sch.id>
2020-10-23 20:48:40 +0700

    Kalkulator buatan sendiri

--- app/src/main/java/com/example/calculate/MainActivity.java
+++ app/src/main/java/com/example/calculate/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import java.util.Scanner;
@@ -28,7 +29,12 @@ public class MainActivity extends AppCompatActivity {
     private Button bagi;
     private Button clear;
     private Button equal;
-    private int val1;
+    private Button sin;
+    private Button cos;
+    private Button tan;
+    private Button square;
+    private String science = "";
+    private double a;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -135,9 +141,38 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        square.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                info.setText(info.getText().toString() + " ^ ");
+            }
+        });
+
+        sin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                science = "sin";
+            }
+        });
+
+        cos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                science = "cos";
+            }
+        });
+
+        tan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                science = "tan";
+            }
+        });
+
         clear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                science = "";
                 info.setText("");
             }
         });
@@ -146,13 +181,19 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 try {
-                int a = evaluteQuestion(info.getText().toString());
-
+                switch (science) {
+                    case "sin": a = Math.sin(Math.toRadians(Double.parseDouble(info.getText().toString()))); break;
+                    case "cos": a = Math.cos(Math.toRadians(Double.parseDouble(info.getText().toString()))); break;
+                    case "tan": a = Math.tan(Math.toRadians(Double.parseDouble(info.getText().toString()))); break;
+                    case "":  a = evaluteQuestion(info.getText().toString()); break;
+                    default: break;
+                        }
                     info.setText(info.getText().toString() + " = " + a);
+
                 }catch (Exception e){}
-            }
-        });
-    }
+            }});
+        }
+
 
     private void setupUIViews() {
 
@@ -173,9 +214,13 @@ public class MainActivity extends AppCompatActivity {
         bagi = (Button) findViewById(R.id.bagi);
         clear = (Button) findViewById(R.id.clear);
         equal = (Button) findViewById(R.id.equal);
+        sin = (Button) findViewById(R.id.sin);
+        cos = (Button) findViewById(R.id.cos);
+        tan = (Button) findViewById(R.id.tan);
+        square = (Button) findViewById(R.id.square);
     }
 
-    public static int evaluteQuestion(String question) {
+    public static double evaluteQuestion(String question) {
         Scanner sc = new Scanner(question);
 
         // get the next number from the scanner
@@ -184,6 +229,7 @@ public class MainActivity extends AppCompatActivity {
         // get everything which follows and is not a number (might contain white spaces)
         String operator = sc.findInLine("[^0-9]*").trim();
         int secondValue = Integer.parseInt(sc.findInLine("[0-9]*"));
+        //Math.sin(Math.toRadians(Double.valueOf(firstValue)));
         switch (operator){
             case "+":
                 return firstValue + secondValue;
@@ -193,8 +239,8 @@ public class MainActivity extends AppCompatActivity {
                 return firstValue / secondValue;
             case "*":
                 return firstValue * secondValue;
-            case "%":
-                return firstValue % secondValue;
+            case "^":
+                return Math.pow(firstValue, secondValue);
             // todo: add additional operators as needed..
             default:
                 throw new RuntimeException("unknown operator: "+operator);
